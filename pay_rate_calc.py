0a4f10e6a8910a50ae34dc154bdf36b4ce786404
bmclay <93384439+bmclay@users.noreply.github.com>
2021-12-13 13:40:51 -0500

    Add files via upload

--- pay_rate_calc.py
+++ pay_rate_calc.py
@@ -5,7 +5,7 @@ pay = 0
 if float(hrs) <= float(40) :
     pay = float(hrs) * float(rate)
 else:
-    othrs = float(hrs) - float(40)
+    othrs = float(hrs) - 40
     otrate = float(rate) / 2 + float(rate)
     pay = float(othrs) * float(otrate) + 40 * float(rate)
 print(pay)        
